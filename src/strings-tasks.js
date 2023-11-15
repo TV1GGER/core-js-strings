94d45c53c1f1e85d2c7cabbf892c173e600526b2
Dzianis Perahudau <tvigger@yandex.ru>
2023-11-15 22:38:35 +0300

    feat: add solution for getStringFromTemplate function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -416,8 +416,9 @@ function invertCase(str) {
  *   getStringFromTemplate('John','Doe') => 'Hello, John Doe!'
  *   getStringFromTemplate('Chuck','Norris') => 'Hello, Chuck Norris!'
  */
-function getStringFromTemplate(/* firstName, lastName */) {
-  throw new Error('Not implemented');
+function getStringFromTemplate(firstName, lastName) {
+  const resultStr = `Hello, ${firstName} ${lastName}!`;
+  return resultStr;
 }
 
 /**
